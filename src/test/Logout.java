ffe5e621dbffe7c6e612ea60ad0cba7a5ee5a5b0
Wu <wuhe76@yahoo.com>
2020-02-29 11:36:45 -0500

    add Logout.java

---
+++ src/test/Logout.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Logout {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        //add more code and update on the following day
+            }
+}
